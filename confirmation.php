a515f42130c80b5e33f020baf753c817cca83eac
Robert Wood Jr <rdrwood@gmail.com>
2019-11-16 20:18:03 -0800

    Add files via upload

--- confirmation.php
+++ confirmation.php
@@ -11,101 +11,138 @@
 <body>
 <h1 class="text-center">Thank you for your entry.</h1>
 <br/>
+<div class="container pb-5" id="main">
+    <h2 class="text-center">Thanks for checking out this site please check out out others including <a href="http://www.rdkwood.com">RDKWOOD.com</a> </h2>
 
-<h2 class="text-center">Thanks for checking out this site please check out out others including <a href="http://www.rdkwood.com">RDKWOOD.com</a> </h2>
+    <?php
 
-
-<?php
-//Connect to db
-require ('/home/rwoodgre/connect.php');
-
-$firstName = $_POST['firstName'];       // required
-$lastName = $_POST['lastName'];         // required
-$title = $_POST['title'];
-$company = $_POST['company'];
-$email = $_POST['email'];               // if  mail is true  required... is supplied must be valid
-$linked = $_POST['linked'];             // if supplied must be valid
-$comment = $_POST['comment'];
-$mail = $_POST['mail'];
-$html = $_POST['html'];
-$text = $_POST['text'];
-$howMet = $_POST['how'];               // is required and must be valid
-$howMet = $_POST['howOther'];
-$mailFormat = "";
-if($mail){
-    $mailFormat = "HTML";
-}
-else{
-    $mailFormat = "Text";
-}
-
-
-$formValues = array(
-    'First Name' => $firstName,
-    'Last Name' => $lastName,
-    "Title" => $title,
-    "Company" => $company ,
-    "Email" => $email,
-    "LinkedIn" => $linked,
-    "Comment" => $comment,
-    "Mailing list email format" => $mailFormat);
-
-
-$isValid = true;
-
-// first name validation
-if($firstName != ""){
-}
-else{
-    echo "<p>First name is required</p>";
-    $isValid = false;
-}
-
-// last name validation
-if($lastName != ""){
-}
-else{
-    echo "<p>Last name is required</p>";
-    $isValid = false;
-}
-
-// email validation
-if ($email != "" && filter_var($email, FILTER_VALIDATE_EMAIL)){
-}
-else{
-    echo "<p>Invalid email format</p>";
-    $isValid = false;
-}
-
-if($isValid) {
-
-    foreach ($formValues as $key => $value) {
-        echo "<tr>";
-        echo "<td><br>";
-        echo $key;
-        echo "</td>";
-        echo "<td><br>";
-        echo $value;
-        echo "<br></td>";
-        echo "</tr>";
-    }
-}
-// require '';
-
-//if($isValid) {
-    $query = "INSERT INTO Guestbook (first_name, last_name, email, title, company, linked, comment)
-                                    VALUES ('$firstName', '$lastName', '$email', '$title', '$company', '$linked', '$comment' )";
-    $Result = mysqli_query($cnxn, $query);
-
-    if( $Result){
-        echo "Data Submitted";
+       //var_dump($_POST);
+    $firstName = $_POST['firstName'];       // required
+    $lastName = $_POST['lastName'];         // required
+    $title = $_POST['title'];
+    $company = $_POST['company'];
+    $email = $_POST['email'];               // if  mail is true  required... is supplied must be valid
+    $linked = $_POST['linked'];             // if supplied must be valid
+    $comment = $_POST['comment'];
+    $mail = $_POST['mail'];
+    $html = $_POST['html'];
+    $text = $_POST['text'];
+    $howMet = $_POST['how'];               // is required and must be valid
+    $howMetOther = $_POST['howOther'];
+    if($mail){
+        $mail = "Yes, please add me to the mailing list";
+        $mailFormat = strtoupper($_POST['method']);
+        $mailing_list = true;
     }
     else{
-        echo "Something Didnt Work";
+        $mail = "No thank you, please do not add me to the mailing list";
+        $mailFormat = "N/A";
+        $mailing_list = false;
     }
-//}
-?>
+    //$mailFormat = strtoupper($_POST['method']);
+    if($howMet === "Other"){
+        $howMet = $howMetOther;
+    }
 
+    $formValues = array(
+        'First Name' => $firstName,
+        'Last Name' => $lastName,
+        "Title" => $title,
+        "Company" => $company ,
+        "Email" => $email,
+        "LinkedIn" => $linked,
+        "Comment" => $comment,
+        "Add to mailing list" => $mail,
+        "Mailing list email format" => $mailFormat,
+        "How we met" => $howMet);
+    $isValid = true;
+    // first name validation
+    if($firstName != ""){
+    }
+    else{
+        echo "<p>First name is required</p>";
+        $isValid = false;
+    }
+
+    // last name validation
+    if($lastName != ""){
+    }
+    else{
+        echo "<p>Last name is required</p>";
+        $isValid = false;
+    }
+
+    // $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    if($mail == "Yes, please add me to the mailing list"){
+        if($email == ""){
+            echo "<p>Email is required to join the mailing list</p>";
+            $isValid = false;
+        }
+        else if ($email != "" && filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+        }
+        else{
+            echo "<p>Invalid email for the mailing list</p>";
+            $isValid = false;
+        }
+    }
+    // email validation
+    if($mail == "No thank you, please do not add me to the mailing list" ){
+        if($email != ""){
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+            }
+            else{
+                echo "<p>Invalid email format</p>";
+                $isValid = false;
+            }
+        }
+
+    }
+    if($howMet == "" || $howMet == 'none'){
+        echo "<p>How met we met is required</p>";
+        $isValid = false;
+    }
+
+    if($linked != ""){
+        if(!filter_var($linked, FILTER_VALIDATE_URL)){
+            echo "<p>Invalid URL for LinkedIn</p>";
+            $isValid = false;
+        }
+    }
+    if($isValid) {
+        foreach ($formValues as $key => $value) {
+            echo "<tr>";
+            echo "<td>";
+            echo $key.":   ";
+            echo "</td>";
+            echo "<td>";
+            if($key == $email){
+                echo "<a href='mailto:$value'>$value</a>";
+            }
+            else{
+                echo $value;
+            }
+            echo "</td></br>";
+            echo "</tr>";
+        }
+    }
+    //Connect to db
+    require ('/home/rwoodgre/connect.php');
+/*    if($isValid) {
+        $query = "INSERT INTO Guestbook 
+                VALUES (now(), '$firstName', '$lastName', '$email', '$title', '$company', '$linked', '$comment', '$howMet', '$mailFormat', '$mailing_list' )";
+        $Result = mysqli_query($cnxn, $query);
+
+        if(mysqli_query($cnxn, $query)){
+            echo "Data Submitted";
+        }
+        else{
+            echo "Something Didnt Work";
+        }
+    }*/
+
+    ?>
+</div>
 
 <!-- jQuery first, then Popper, then Bootstrap, then developer made -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
