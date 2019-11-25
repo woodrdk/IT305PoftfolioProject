6d5ed6d99be4b1c9aa4d14ee66de3396655adcd8
Robert Wood Jr <rdrwood@gmail.com>
2019-11-24 22:06:11 -0800

    Add files via upload

--- confirmation.php
+++ confirmation.php
@@ -1,17 +1,88 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Rob's Guestbook Confirmation</title>
+
     <!-- FAVICON -->
     <link rel="icon" type="image/jpg" href="images/gears.ico">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Material Design Bootstrap -->
+    <link type="text/css" href="styles/mdb.min.css" rel="stylesheet">
+    <!-- .css -->
+    <link rel="stylesheet" type="text/css" href="styles/port.css">
+    <link rel="stylesheet" type="text/css" href="styles/bookCSS.css">
+    <style>
+        #footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+
+        }
+        body{
+            background-color: lightgrey;
+        }
+        .req{
+            color: red;
+        }
+    </style>
+    <!-- Material Design Bootstrap -->
+    <link type="text/css" href="styles/mdb.min.css" rel="stylesheet">
+    <!-- .css -->
+    <link rel="stylesheet" type="text/css" href="styles/port.css">
+
 </head>
 <body>
-<h1 class="text-center">Thank you for your entry.</h1>
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark sticky-top">
+    <a class="navbar-brand" href="#">RW</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://rwood.greenriverdev.com/305/resume/index.html">Resume</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Portfolio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="guestbook.html">Guestbook</a>
+            </li>
+            <li>
+                <a class="nav-link" href="admin.php">Admin</a>
+            </li>
+
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="https://www.linkedin.com/in/robert-wood-jr-200180161/" target="_blank">LinkedIn</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://github.com/woodrdk" target="_blank">GitHub</a>
+            </li>
+        </ul>
+    </div>
+
+</nav>
+
+
 <br/>
 <div class="container pb-5" id="main">
+    <div>
+        <h1 class="text-center p-5">Thank you for your entry.</h1>
+    </div>
+
     <h2 class="text-center">Thanks for checking out this site please check out out others including <a href="http://www.rdkwood.com">RDKWOOD.com</a> </h2>
 
     <?php
@@ -128,21 +199,36 @@
     }
     //Connect to db
     require ('/home/rwoodgre/connect.php');
-/*    if($isValid) {
-        $query = "INSERT INTO Guestbook 
-                VALUES (now(), '$firstName', '$lastName', '$email', '$title', '$company', '$linked', '$comment', '$howMet', '$mailFormat', '$mailing_list' )";
+    if($isValid) {
+        $query = "INSERT INTO Guestbook (first_name, last_name, title, company, email, mailing_list, mail_list_format, linkedIn, comment, how_we_met, added)
+                VALUES ( '$firstName', '$lastName', '$title', '$company', '$email',  '$mailing_list', '$mailFormat', '$linked', '$comment', '$howMet', now() )";
         $Result = mysqli_query($cnxn, $query);
 
-        if(mysqli_query($cnxn, $query)){
-            echo "Data Submitted";
+        if($Result){
+            echo "Thank you for your submission, your data was submitted";
         }
         else{
-            echo "Something Didnt Work";
+            echo "Something Didnt Work please try again";
         }
-    }*/
+    }
 
     ?>
 </div>
+
+<!--<div class="right-stick">
+    <a href="#">Let's work together!</a>
+</div>-->
+<nav id="footer" class="navbar navbar-dark bg-dark">
+    <div class="col-4 text-light"> &copy; October 2019 </div>
+    <div>
+        <i class="fa fa-github" style="font-size:48px;color:white"></i>
+        <i class="fa fa-linkedin-square" style="font-size:48px;color:dodgerblue"></i>
+        <i class="fa fa-envelope" style="font-size:48px;color:white"></i>
+        <!--<i class='fab fa-adobe' style='font-size:48px;color:red'></i>-->
+        <!--<i class='fab fa-accessible-icon' style='font-size:48px;color:blue'></i>-->
+    </div>
+
+</nav>
 
 <!-- jQuery first, then Popper, then Bootstrap, then developer made -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
