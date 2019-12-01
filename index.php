5ae50e2f332b8f6688e5b47af37da059ce7f3486
Robert Wood Jr <rdrwood@gmail.com>
2019-11-30 21:27:07 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
+<?php
+    //Start the session
+    session_start();
+?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Rob Wood's Portfolio</title>
-
     <!-- FAVICON -->
     <link rel="icon" type="image/jpg" href="images/gears.ico">
     <!-- Font Awesome -->
@@ -14,10 +16,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
     <!-- Material Design Bootstrap -->
     <link type="text/css" href="styles/mdb.min.css" rel="stylesheet">
-    <!-- .css -->
     <link rel="stylesheet" type="text/css" href="styles/port.css">
     <style>
         .footer {
@@ -43,67 +43,65 @@
     </style>
 </head>
 <body>
-<div id="nav-container-top"></div>
-<div class="container-fluid d-none d-lg-block" id="codeWriter">
-    <div class="row">
-        <div class="col-xs-1">
-            <pre id="typewriter">
-                <span class="var-highlight">var</span> object = {
-                    name: <span class="string-highlight">'Robert Wood Jr.'</span>,
-                    type: <span class="string-highlight">'Software Developer'</span>,
-                    location: <span class="string-highlight">'Spanaway, Washington'</span>,
-                    properties:[<span class="string-highlight">'Javascript'</span>,
-                    <span class="string-highlight">'HTML'</span>,
-                    <span class="string-highlight">'CSS'</span>,
-                    <span class="string-highlight">'PHP'</span>,
-                    <span class="string-highlight">'SQL'</span>,
-                    <span class="string-highlight">'C#'</span>,
-                    <span class="string-highlight">'JAVA'</span>,
-                    <span class="string-highlight">'and more...'</span>];
-                };
-
-            </pre>
+    <div id="nav-container-top"></div>
+    <div class="container-fluid d-none d-lg-block" id="codeWriter">
+        <div class="row">
+            <div class="col-xs-1">
+                <pre id="typewriter">
+                    <span class="var-highlight">var</span> object = {
+                        name: <span class="string-highlight">'Robert Wood Jr.'</span>,
+                        type: <span class="string-highlight">'Software Developer'</span>,
+                        location: <span class="string-highlight">'Spanaway, Washington'</span>,
+                        properties:[<span class="string-highlight">'Javascript'</span>,
+                        <span class="string-highlight">'HTML'</span>,
+                        <span class="string-highlight">'CSS'</span>,
+                        <span class="string-highlight">'PHP'</span>,
+                        <span class="string-highlight">'SQL'</span>,
+                        <span class="string-highlight">'C#'</span>,
+                        <span class="string-highlight">'JAVA'</span>,
+                        <span class="string-highlight">'and more...'</span>];
+                    };
+                </pre>
+            </div>
         </div>
     </div>
-</div>
-<div class="footer  text-center d-none d-lg-block">
-    <button onclick="skip()" id="skip">Learn More</button>
-</div>
-<div id="preType" hidden >
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark sticky-top">
-        <a class="navbar-brand" href="index.php">RW</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#contact">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="http://rwood.greenriverdev.com/305/resume/index.html"  target="_blank">Resume</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#projects">Portfolio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="guestbook.html">Guestbook</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="login.php">Admin</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="https://www.linkedin.com/in/robert-wood-jr-200180161/" target="_blank">LinkedIn</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="https://github.com/woodrdk" target="_blank">GitHub</a>
-                </li>
-            </ul>
-        </div>
-
-</nav>
+    <div class="footer  text-center d-none d-lg-block">
+        <button onclick="skip()" id="skip">Skip Animation & Learn More</button>
+    </div>
+    <div id="preType" hidden >
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark sticky-top">
+            <a class="navbar-brand" href="index.php">RW</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="http://rwood.greenriverdev.com/305/resume/index.html"  target="_blank">Resume</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#projects">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="guestbook.html">Guestbook</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Admin</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.linkedin.com/in/robert-wood-jr-200180161/" target="_blank">LinkedIn</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://github.com/woodrdk" target="_blank">GitHub</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
 
     <div class="container-fluid w-responsive mx-auto mb-5"><br>
         <div class="row">
@@ -111,20 +109,18 @@
                 <img id="myPic2" class=" col rounded-circle" src="images/profile.jpg" alt="Rob's Profile picture">
             </div>
             <div class="col-md-8" id="aboutMe">
-                Thank you for taking the time to learn more about me and view my portfolio.
-                My name is Robert Wood and I create quality applications, web, desktop and mobile.
+                Thank you for taking the time to learn more about me and my work.
+                My name is Robert Wood and I create quality web, desktop and mobile applications.
                 I enjoy using my technical knowledge to bring people's ideas to life and create useful functional software
                 products for the world. I have worked on both independent and team projects playing a role in helping develop
-                successful creations multiple clients. My area of skill may exist in one area, but I can adapt and overcome
+                successful creations of multiple clients. My skill strengths may exist in one area, but I can adapt and overcome
                 gaps of my skill by self learning. I enjoy working on things that others tend to struggle with even if I am
                 not super comfortable in that area myself. These type of projects push me to broaden my skills and knowledge
-                even more, than I already do and step up to make sure the job gets done and done correctly. I have worked in
+                even more than I already do and allows me to step up to make sure the job gets done and done correctly. I have worked in
                 both waterfall and agile enviroments and am constantly learning about new methods to do things. If you are
                 interested in learning more about me or my projects and skills, please feel free to contact me directly.
             </div>
-
         </div>
-
         <div class="text-center" id="projects">
             <hr>
             <h1>Projects</h1>
@@ -350,12 +346,10 @@
         </div>
     </div>
 </div>
-        <!-- icons-->
 
     <div class="right-stick">
-        <a href="#">Let's work together!</a>
+        <a href="mailto:rdrwood@gmail.com">Let's work together!</a>
     </div>
-
     <nav id="footer" class="navbar navbar-dark bg-dark">
             <div class="col-4 text-light"> &copy; October 2019 </div>
             <div>
@@ -365,7 +359,6 @@
                 <!--<i class='fab fa-adobe' style='font-size:48px;color:red'></i>-->
                 <!--<i class='fab fa-accessible-icon' style='font-size:48px;color:blue'></i>-->
             </div>
-
     </nav>
 </div>
     <!-- optional JavaScript -->
@@ -377,34 +370,19 @@
 <!-- MDB core JavaScript -->
 <script type="text/javascript" src="scripts/mdb.min.js"></script>
 <script>
-
-    var observer = new IntersectionObserver(function(entries) {
-        if(entries[0].intersectionRatio === 0)
-            document.getElementById("#right-stick").style.display = "block";
-        else if(entries[0].intersectionRatio === 1)
-            document.getElementById("#right-stick").style.display = "none";
-    }, { threshold: [0,1] });
-
-    observer.observe(document.querySelector("#nav-container-top"));
-
     function setupTypewriter(t) {
         let HTML = t.innerHTML;
-
         t.innerHTML = "";
-
         let cursorPosition = 0,
             tag = "",
             writingTag = false,
             tagOpen = false,
             typeSpeed = 1,
             tempTypeSpeed = 0;
-
         let type = function() {
-
             if (writingTag === true) {
                 tag += HTML[cursorPosition];
             }
-
             if (HTML[cursorPosition] === "<") {
                 tempTypeSpeed = 0;
                 if (tagOpen) {
@@ -440,20 +418,16 @@
                 }
             }
             cursorPosition += 1;
-            //alert(cursorPosition);
             if (cursorPosition < HTML.length - 1) {
                 setTimeout(type, tempTypeSpeed);
             }
-
             if(cursorPosition === 907){
                skip();
             }
         };
-
         return {
             type: type
         };
-
     }
     function skip(){
         document.getElementById("preType").hidden = false;
@@ -469,12 +443,9 @@
         let pageLoad = document.getElementById("preType").hidden = false;
     }
     // https://codepen.io/stevn/pen/jEZvXa
-    /// diag the css fail
-
     if ($(window).width() < 960) {
         skip();
     }
 </script>
-
 </body>
 </html>
